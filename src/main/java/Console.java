e6e671faee0d4471a2c16d2a9a76059a20c3bd1e
Gennadiy <Relect@bk.ru>
2020-06-03 19:39:25 +0300

    работает ввод в массив key, value, и вывод

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -7,9 +7,34 @@ public class Console {
         try {
             Scanner in = new Scanner(System.in);
             String json = in.nextLine();
-            if (!json.startsWith("{") || !json.endsWith("}") || (json.indexOf(':') == -1)) throw new InputMismatchException();
+            if (!json.startsWith("{\"") ||
+                    !json.endsWith("\"}") ||
+                    (json.indexOf(':') == -1)) throw new InputMismatchException();
 
-            
+            String json1 = json.substring(1);
+            String [] key = new String[100];
+            String [] value = new String[100];
+            int i = 0;
+
+            while (!json1.startsWith("}")){
+                key[i] = json1.substring(1,json1.indexOf(':')-1);
+
+                if (json1.indexOf(',') != -1) {
+                    value[i] = json1.substring(json1.indexOf(':') + 2, json1.indexOf(',') - 1);
+                    json1 = json1.substring(json1.indexOf(',') + 1);
+                } else {
+                    value[i] = json1.substring(json1.indexOf(':') + 2, json1.indexOf('}') - 1);
+                    json1 = json1.substring(json1.indexOf('}'));
+                }
+                i++;
+            }
+            System.out.println(json1);
+            for (i = 0; i < key.length; i++) {
+                if (key[i] != null) {
+                    System.out.println(key[i]);
+                    System.out.println(value[i]);
+                }
+            }
         } catch (InputMismatchException e) {
             System.out.println("JSON должен начинаться с \"{\",  заканчиваться \"}\" и иметь \":\". ");
         }
